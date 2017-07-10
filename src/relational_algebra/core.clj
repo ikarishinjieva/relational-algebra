7163a3f8047823e5f7812ef92c16cf3634dcde6d
Tachikoma <ikari.shinji.eva@gmail.com>
2017-07-10 23:53:37 +0800

    change to single-op per relation, instead of multi-op per relation

--- src/relational_algebra/core.clj
+++ src/relational_algebra/core.clj
@@ -1,62 +1,47 @@
 (ns relational-algebra.core
   (:require [clojure.string :as str]))
 
-(defprotocol IRelation
-  (base [this])
-  (projection [this]))
-
-(deftype Relation [base projection]
-  IRelation
-  (base [_] base)
-  (projection [_] projection))
-
-(defrecord Projection [columns])
-
-(defmacro defrelation [relation-name base]
-  `(def ~relation-name (Relation. ~base (->Projection '[]))))
-
-;;;;;; convenience methods
-
-(defn project [relation columns]
-  (let [base (base relation)
-        projection (->Projection columns)]
-    (Relation. base projection)))
-
-;;;;;; to-sql
-
 (defmulti to-sql type)
+(defmulti query-sql (fn [rel data] (type rel)))
+
+(defprotocol IRelation
+  (sql [this])
+  (query [this data]))
+
+(defrecord Base [tbl]
+  IRelation
+  (sql [_] 
+          (name tbl))
+  (query [_ data] 
+          (tbl data))
+  )
+
+(defrecord Project [tbl cols]
+  IRelation
+  (sql [_] 
+          (let [cols-str (str/join ", " (map name cols))]
+            (str "SELECT " cols-str " FROM " (to-sql tbl))
+            ))
+  (query [_ data] 
+          (let [tbl-data (query-sql tbl data)]
+            (map #(select-keys % cols) tbl-data)
+            )))
+
+(defrecord Select [tbl condition]
+  IRelation
+  (sql [_]
+          (let 
+            [cond-sql-in-middle-seq (map #(to-sql (nth condition %)) '(1 0 2))
+             cond-str (str/join " " cond-sql-in-middle-seq)
+             tbl-str (to-sql tbl)]
+             (str "SELECT * FROM " tbl-str " WHERE " cond-str)
+            )))
 
 (defmethod to-sql clojure.lang.Keyword [k] (name k))
+(defmethod to-sql Base [base] (sql base))
+(defmethod to-sql Project [project] (sql project))
+(defmethod to-sql Select [select] (sql select))
+(defmethod to-sql java.lang.Long [long] (str long))
 
-(defmethod to-sql Projection [p] 
-  (let [cols (:columns p)
-        sql-cols (if (empty? cols) "*" (str/join ", " (map #(name %) cols)))
-        ]
-    (str "SELECT " sql-cols)
-    ))
-
-(defmethod to-sql Relation [relation]
-  (let [base (base relation)
-        projection (projection relation)
-        select-clause (to-sql projection)]
-    (str select-clause " FROM " (to-sql base))))
-
-;;;;;; query-sql
-
-(defmulti query-sql (fn [relation data] (type relation)))
-
-(defmethod query-sql clojure.lang.Keyword [k data] (get data k))
-
-(defmethod query-sql Projection [p data] 
-  (let [cols (:columns p)]
-    (if (empty? cols) 
-      data 
-      (map #(select-keys % cols) data))))
-
-(defmethod query-sql Relation [relation data] 
-  (let [base (base relation)
-        projection (projection relation)
-        base-data (query-sql base data)
-        project-data (query-sql projection base-data)
-        ]
-    (identity project-data)))
+(defmethod query-sql Base [base data] (query base data))
+(defmethod query-sql Project [project data] (query project data))
--- test/relational_algebra/core_test.clj
+++ test/relational_algebra/core_test.clj
@@ -2,38 +2,49 @@
   (:require [clojure.test :refer :all]
             [relational-algebra.core :refer :all]))
 
-(defrelation person :tbl_person)
+(def person (->Base :tbl_person))
 
-(def data {:tbl_person 
-           '(
-             {:id 1, :name "alex"}
-             {:id 2, :name "alexon"}
-             )})
+(def data {
+           :tbl_person '(
+                          {:id 1 :name "alex"}
+                          {:id 2 :name "alexon"}
+                          {:id 3 :name "richard"}
+                          )})
 
 (deftest to-sql-base
   (let [
         actual (to-sql person)
-        expect "SELECT * FROM tbl_person"] 
+        expect "tbl_person"] 
+    (is (= expect actual))
+    ))
+
+(deftest to-sql-project
+  (let [
+        proj (->Project person '(:id :name))
+        actual (to-sql proj)
+        expect "SELECT id, name FROM tbl_person"] 
+    (is (= expect actual))
+    ))
+
+(deftest to-sql-select
+  (let [
+        proj (->Select person '(:> :id 10))
+        actual (to-sql proj)
+        expect "SELECT * FROM tbl_person WHERE id > 10"] 
     (is (= expect actual))
     ))
 
 (deftest query-sql-base
   (let [
         actual (query-sql person data)
-        expect (get data :tbl_person)] 
+        expect (:tbl_person data)] 
     (is (= expect actual))
     ))
 
-(deftest to-sql-projection
+(deftest query-sql-project
   (let [
-        actual (to-sql (project person '[:id :name]))
-        expect "SELECT id, name FROM tbl_person"] 
-    (is (= expect actual))
-    ))
-
-(deftest query-sql-projection
-  (let [
-        actual (query-sql (project person '[:id]) data)
-        expect '({:id 1} {:id 2})] 
+        proj (->Project person '(:id))
+        actual (query-sql proj data)
+        expect '({:id 1} {:id 2} {:id 3})] 
     (is (= expect actual))
     ))
