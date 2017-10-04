81d41b9005cbdff02bb01670f2571c015993a5a5
Tachikoma <ikari.shinji.eva@gmail.com>
2017-10-04 23:14:05 +0800

    Add logger

--- project.clj
+++ project.clj
@@ -3,7 +3,15 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]]
+  :dependencies [[org.clojure/clojure "1.8.0"] 
+                 [org.clojure/tools.logging "0.4.0"]
+                 [log4j/log4j "1.2.16" :exclusions [javax.mail/mail
+                                                  javax.jms/jms
+                                                  com.sun.jdmk/jmxtools
+                                                  com.sun.jmx/jmxri]]]
   :main ^:skip-aot relational-algebra.core
   :target-path "target/%s"
-  :profiles {:uberjar {:aot :all}})
+  :profiles {:uberjar {:aot :all}}
+  :mirrors {"central" {:name "central"
+                       :url "http://maven.aliyun.com/nexus/content/groups/public/"}}
+  )
